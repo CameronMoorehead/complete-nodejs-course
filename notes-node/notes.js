06b52bc3879cfe628147c61dab0293cdac22b348
Cameron <mooreheadc@hotmail.com>
2017-07-12 22:15:55 -0700

    finished notes; added weather-app dir

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,44 +1,59 @@
-console.log("Starting notes.js")
-
 const fs = require("fs")
 
+let fetchNotes = () => {
+  try {
+    let notesString = fs.readFileSync("notes-data.json")
+    return JSON.parse(notesString)
+  } catch(e) {
+    return []
+  }
+}
+
+let saveNotes = notes => {
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes))
+}
+
 let addNote = (title, body) => {
-  let notes = []
+  let notes = fetchNotes()
   let note = {
     title,
     body
   }
-
-  try {
-    let notesString = fs.readFileSync("notes-data.json")
-    notes = JSON.parse(notesString)
-  } catch(e) {
-
-  }
-
   let duplicateNotes = notes.filter(note => note.title === title)
 
   if (duplicateNotes.length === 0) {
     notes.push(note)
-    fs.writeFileSync("notes-data.json", JSON.stringify(notes))
+    saveNotes(notes)
+    return note
   }
 }
 
 let getAll = () => {
-  console.log("Getting all notes...")
+  return fetchNotes()
 }
 
-let getNote = (title) => {
-  console.log("Getting note", title)
+let getNote = title => {
+  let notes = fetchNotes()
+  let noteToRead = notes.filter(note => note.title === title)
+  return noteToRead[0]
 }
 
-let removeNote = (title) => {
-  console.log("Removing note", title)
+let removeNote = title => {
+  let notes = fetchNotes()
+  let filteredNotes = notes.filter(note => note.title !== title)
+  saveNotes(filteredNotes)
+
+  return notes.length !== filteredNotes.length
+}
+
+let logNote = (note) => {
+  console.log(`\n Title: ${note.title} \n Body: ${note.body}`);
 }
 
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 }
---
+++ notes-node/playground/arrow-function.js
@@ -0,0 +1,16 @@
+let square = x  => x * x
+console.log(square(9));
+
+let user = {
+  name: "Cameron",
+  sayHi: () => {
+    console.log(`Hi. I'm ${this.name}`)
+  },
+  sayHiAlt () {
+    console.log(arguments)
+    console.log(`Hi. I'm ${this.name}`)
+  }
+}
+
+user.sayHi()
+user.sayHiAlt(1,2,3)
