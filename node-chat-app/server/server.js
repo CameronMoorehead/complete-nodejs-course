67bc0a900e706ccefeb403ccef447f441b5ee3c7
Cameron <mooreheadc@hotmail.com>
2017-08-21 18:50:35 -0700

    initial commit

---
+++ node-chat-app/server/server.js
@@ -0,0 +1,16 @@
+const path = require("path")
+const express = require("express")
+
+const publicPath = path.join(__dirname, "../public")
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(publicPath))
+
+// app.get("/", (req, res) => {
+//   res.send(publicPath + "/index.html")
+// })
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${port}`)
+})
