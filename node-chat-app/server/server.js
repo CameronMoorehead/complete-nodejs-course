a9d08c0fa8997b35a9f89ae69e52aa873b2a0ca3
Cameron <mooreheadc@hotmail.com>
2017-08-22 19:14:24 -0700

    Add newMessage and createMessage events

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -14,6 +14,22 @@ app.use(express.static(publicPath))
 io.on("connection", socket => {
   console.log("New user connected")
 
+  socket.emit("newEmail", {
+    from: "mike@example.com",
+    text: "Hey, what's up?",
+    createdAt: 123
+  })
+
+  socket.emit("newMessage", {
+    from: "Cameron",
+    text: "Yo!",
+    createdAt: 456
+  })
+
+  socket.on("createMessage", message => {
+    console.log("createMessage", message)
+  })
+
   socket.on("disconnect", () => {
     console.log("User was disconnected")
   })
