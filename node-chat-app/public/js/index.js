c5f86d63747cc0bf553ed16cd71e992bed17f8ba
Cameron <mooreheadc@hotmail.com>
2017-08-24 18:00:37 -0700

    emit new message on createMessage

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -1,19 +1,10 @@
 var socket = io()
 socket.on("connect", function() {
   console.log("Connected to server")
-
-  socket.emit("createMessage", {
-    from: "Alycia",
-    text: "Hello thar! :)"
-  })
 })
 
 socket.on("disconnect", function() {
   console.log("Disconnected from server")
-})
-
-socket.on("newEmail", function(email) {
-  console.log("New email", email)
 })
 
 socket.on("newMessage", function(message) {
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -14,20 +14,13 @@ app.use(express.static(publicPath))
 io.on("connection", socket => {
   console.log("New user connected")
 
-  socket.emit("newEmail", {
-    from: "mike@example.com",
-    text: "Hey, what's up?",
-    createdAt: 123
-  })
-
-  socket.emit("newMessage", {
-    from: "Cameron",
-    text: "Yo!",
-    createdAt: 456
-  })
-
   socket.on("createMessage", message => {
     console.log("createMessage", message)
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   })
 
   socket.on("disconnect", () => {
